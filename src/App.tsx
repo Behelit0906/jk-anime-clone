fd6df1abe01a5814dc79fe50e62f93f4825c8d1a
Luis Salcedo <luis.salcedo0906@gmail.com>
2024-02-09 18:02:31 -0500

    refactoring app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,21 @@
-// import LinkComponent from "./components/LinkComponent"
-// import Icon from "./components/Icon";
-// import SearchBar from "./components/SearchBar";
-// import SearchList from "./components/SearchList";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-
+import {Routes , Route } from "react-router-dom" 
+import Home from "./pages/Home";
+import { useEffect } from "react";
 
 const App = () => {
+    useEffect(() => {
+      const darkMode = localStorage.getItem('darkMode');
+      if(darkMode === 'true') {
+        document.getElementById('root')?.classList.add('dark');
+      } 
+    }, [])
+
     return (
-        // <article className="h-[150px] p-5 flex items-center gap-x-5 bg-blue-50 font-mulish font-bold text-white">
-        //     <Icon />
-        //     <LinkComponent link="./" padding="py-[15px] px-[19px]">
-        //         Directorio
-        //     </LinkComponent>
-        //     <LinkComponent link="./" padding="py-[15px] px-[19px]">
-        //         Horario
-        //     </LinkComponent>
-        //     <LinkComponent link="./" padding="py-[15px] px-[19px]">
-        //         Top
-        //     </LinkComponent>
-        //     <SearchBar />
-        //     <div>
-            
-        // </article>
-        // <div className="p-10 h-screen bg-blue-100">
-        //     <SearchList animes={null} />
-        // </div>
-        <div className="bg-[#F7F7F7] h-screen flex flex-col justify-between">
-            <Header />
-            <Footer />
-        </div>
-        
+      <>
+        <Routes>
+          <Route path="/" Component={Home} />
+        </Routes>
+      </>  
     )
 }
 
