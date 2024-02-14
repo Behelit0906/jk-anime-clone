5b33e6cd7c363d49e1d3d99f42397ed2d0893f75
Luis Salcedo <luis.salcedo0906@gmail.com>
2024-02-14 11:28:31 -0500

    adjust route link in page icon

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -3,7 +3,7 @@ import logo from '../assets/logo.png';
 
 function Icon() {
   return (
-    <Link to="./">
+    <Link to="/">
       <img className="w-[165px] lg:w-[130px] xl:w-[165px]" src={logo} alt="JK Anime logo" />
     </Link>
   )
