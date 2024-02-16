8c5d4aa0f968d4afc05746270d9687d0d0ebb646
Luis Salcedo <luis.salcedo0906@gmail.com>
2024-02-15 19:59:47 -0500

    adjust status prop

--- src/components/AddAnimeCard.tsx
+++ src/components/AddAnimeCard.tsx
@@ -10,6 +10,12 @@ interface Props {
 }
 
 function AddAnimeCard(props:Props) {
+
+  let status = '';
+  if(props.status === 'Not yet aired') status = 'Upcoming';
+  else if(props.status === 'Currently Airing') status = 'Airing'
+  else status = 'Finished'
+
   return (
     <Link to='/' className="special-card block relative w-full h-full" >
       <div className="bg-image relative bg-image w-full h-[255px] rounded-lg overflow-hidden" style={{
@@ -25,7 +31,7 @@ function AddAnimeCard(props:Props) {
       <div>
         <div className="pl-2 py-2 flex flex-nowrap">
           <span className="py-[1px] px-[10px] rounded-xl font-mulish font-bold bg-myOrange-50 text-white text-[10px] mr-1">
-            {props.status.split(' ')[1]}
+            {status}
             </span>
           <span className="py-[1px] px-[10px] rounded-xl font-mulish font-bold bg-blue-100 text-white text-[10px]">{props.type}</span>
         </div>
