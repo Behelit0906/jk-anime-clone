2c8243549dd8041cae4740176e7e20a907f1c67b
Luis Salcedo <luis.salcedo0906@gmail.com>
2024-02-15 20:00:25 -0500

    add list of letters to directory page

---
+++ src/components/DirectoryComponents/DirectoryFilter.tsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+
+function DirectoryFilter() {
+  const letters = 'ABCDEFGHIJKLMOPQRSTUVWXYZ'.split('');
+
+  return (
+    <ul className="flex justify-center flex-wrap jus p-2 bg-[#607d8b] rounded-xl dark:bg-dark-100">
+      {
+        letters.map((letter, key) => 
+          <li key={key} className="text-white hover:bg-white hover:text-[#607d8b]">
+            <Link to={`/letter/${letter}`} className="block p-[10px]">{letter}</Link>
+          </li>
+        )
+      }
+    </ul>
+  )
+}
+
+export default DirectoryFilter;
