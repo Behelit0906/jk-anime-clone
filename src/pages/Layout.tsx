439f3c5a8d803d0c9c39cca053489bd013a06b38
Luis Salcedo <luis.salcedo0906@gmail.com>
2024-02-20 11:29:53 -0500

    add minimum height to layout

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -10,7 +10,7 @@ function Layout(props:Props) {
   return (
     <div className="flex flex-col bg-[#F7F7F7] dark:bg-dark-150">
       <Header/>
-      <main className="flex flex-col items-center">
+      <main className="flex flex-col items-center min-h-screen">
         {props.children}
       </main>
       <Footer />
