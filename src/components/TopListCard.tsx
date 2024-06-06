63fec4f169f32e453c040653079d078d518074df
Luis Salcedo <luis.salcedo0906@gmail.com>
2024-06-06 14:57:24 -0500

    adjust border radius

--- src/components/TopListCard.tsx
+++ src/components/TopListCard.tsx
@@ -23,7 +23,7 @@ function TopListCard(props:Props) {
     }}>
       <span className={`absolute top-2 left-3 px-3 ${props.rankColor} rounded font-[13px] font-mulish text-white`}>{props.rank}</span>
       <span className={`absolute bottom-0 left-0 w-full py-1 px-2 font-mulish ${props.fontSize ? props.fontSize: ''} bg-[#3D3D3D] text-white rounded-[10px]`}>{props.title}</span>
-      <span className="score flex justify-center items-center absolute z-30 w-full h-0 opacity-85 bg-myOrange-50">
+      <span className="score flex justify-center items-center absolute z-30 w-full h-0 opacity-90 bg-myOrange-50 rounded-[10px]">
         <span className="topCardContent text-white font-mulish font-bold opacity-0 transition-opacity duration-100">
           <AiFillLike className="text-white w-10 h-10" />
           {props.score}
