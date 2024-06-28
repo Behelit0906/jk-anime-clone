e23e5a21bd1166e217a57657028c8fa7e6fb3d56
Luis Salcedo <luis.salcedo0906@gmail.com>
2024-06-28 14:43:45 -0500

    refactor component

--- src/components/TopPageComponents/LocationString.tsx
+++ src/components/TopPageComponents/LocationString.tsx
@@ -3,22 +3,44 @@ import { Link } from "react-router-dom";
 import { FaHome } from "react-icons/fa";
 import { IoIosArrowForward } from "react-icons/io";
 
+function seasonMessage () {
+  let message = '';
+  const currentDate = new Date();
+  const currentMonth = currentDate.getUTCMonth();
+
+  if(currentMonth >= 3 || currentMonth <= 5)
+    message = `Spring season schedule ${currentDate.getUTCFullYear()} ( April - May - June )`
+  if(currentMonth >= 0 || currentMonth <= 2)
+    message = `Winter season schedule ${currentDate.getUTCFullYear()} ( January - February - March )`
+  if(currentMonth >= 6 || currentMonth <= 8)
+    message = `Summer season schedule ${currentDate.getUTCFullYear()} ( July - August - September )`
+  if(currentMonth >= 9 || currentMonth <= 11)
+    message = `Fall season schedule ${currentDate.getUTCFullYear()} ( October - November - December )`
+  
+  return message
+}
 
 function LocationString() {
   const location = useLocation();
 
-  const pageName = location.pathname.includes('/top') ? 'Current top' : 'Ranking global';
+  const pageName = location.pathname.includes('/top') ? 'Current top' : 'Schedules';
 
   return (
-    <div className="flex gap-1">
+    <div className="flex flex-wrap gap-1">
       <Link className="flex gap-1 items-center font-mulish text-[15px] dark:text-white" to={'/'}>
         <FaHome className="mr-2" />
           Home
         <IoIosArrowForward />
       </Link>
-        <span className="font-mulish text-[15px] text-myOrange-50">     
+        <span className={`font-mulish text-[15px] ${location.pathname.includes('/top') ? 'text-myOrange-50' : 'dark:text-white'}`}>     
           { pageName }
         </span>
+        {
+          location.pathname.includes('/schedules') && 
+          <span className="flex items-center text-myOrange-50 text-[15px]">
+            <IoIosArrowForward className="mr-1 text-black dark:text-white" /> {seasonMessage()}
+          </span>
+        }
     </div>
   )
 }
